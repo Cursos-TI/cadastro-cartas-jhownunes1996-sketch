6b2ca4388860c3c53129431eae55d1a323202a47
jhownunes1996-sketch <jhownunes1996@gmail.com>
2026-04-15 15:57:51 -0300

    Estácio CartasSuperTrunfo.c
    Trabalho Estácio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,12 +4,62 @@
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+int main()
+   {
+char carta, cidade, estado ; //inserindo variaveis tipo caractere
+int codigo, populacao, area, pib, turisticos; // inserindo variaveis tipo inteiro
 
-  // Área para entrada de dados
+printf("carta 1 \n\n"); // dados da carta 1
 
-  // Área para exibição dos dados da cidade
+printf("Estado: ",estado); //impressão
+scanf("%c", &estado);      //declaração de variáveis
 
-return 0;
-} 
+printf("codigo: ",codigo);
+scanf("%d", &codigo);
+
+printf("cidade: ",cidade);
+scanf(" %c", &cidade);
+
+printf("Populacao: ");
+scanf("%d", &populacao);
+
+printf("Area: ");
+scanf("%f", &area);
+
+printf("PIB: ");
+scanf("%f", &pib);
+
+printf("Numero de Pontos Turisticos: ");
+scanf("%d", &turisticos);
+
+
+//Agora vai ser inserido os dados da carta 2
+//=========================================
+
+
+printf("\n\ncarta 2 \n\n"); //dados da carta 2
+
+printf("Estado: ",estado);  //impressão
+scanf("%c", &estado);       //declaração de variáveis
+
+printf("codigo: ",codigo);
+scanf("%d", &codigo);
+
+printf("cidade: ",cidade);
+scanf(" %c", &cidade);
+
+printf("Populacao: ");
+scanf("%d", &populacao);
+
+printf("Area: ");
+scanf("%f", &area);
+
+printf("PIB: ");
+scanf("%f", &pib);
+
+printf("Numero de Pontos Turisticos ");
+scanf("%d", &turisticos);
+
+    return 0; //função retorna um valor 
+
+   }
